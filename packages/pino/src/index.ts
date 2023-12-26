1b997ca5733e0acbb0ca0df125604de8343992db
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2023-12-25 21:27:56 -0500

    chore: format

--- packages/pino/src/index.ts
+++ packages/pino/src/index.ts
@@ -4,25 +4,19 @@ import { AdapterOptions } from "./types";
 async function createTransport(options: AdapterOptions) {
 	return build(async (source) => {
 		for await (const obj of source) {
-			const request = await fetch(
-				options.apiURL ?? "https://loggregator.co/api/logger",
-				{
-					method: "POST",
-					body: JSON.stringify({ content: obj.msg }),
-					headers: {
-						"Content-Type": "application/json",
-						Authorization: `Bearer ${options.token}`,
-					},
+			const request = await fetch(options.apiURL ?? "https://loggregator.co/api/logger", {
+				method: "POST",
+				body: JSON.stringify({ content: obj.msg }),
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${options.token}`,
 				},
-			);
+			});
 
-			const body = (await request.json()) as
-				| { success: true }
-				| { success: false; error: string };
+			const body = (await request.json()) as { success: true } | { success: false; error: string };
 
 			const success = body.success;
-			if (!success)
-				console.warn("[PINO-LOGGREGATOR] request to API failed!", body.error);
+			if (!success) console.warn("[PINO-LOGGREGATOR] request to API failed!", body.error);
 		}
 	});
 }
