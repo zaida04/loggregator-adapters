ea1d76644684e0b6f3e0ef122385f6a03d5182cb
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2023-12-25 21:11:44 -0500

    feat: ready for prod

--- packages/pino/src/index.ts
+++ packages/pino/src/index.ts
@@ -1,11 +1,28 @@
 import build from "pino-abstract-transport";
-import { sendLog } from "./request";
 import { AdapterOptions } from "./types";
 
 async function createTransport(options: AdapterOptions) {
 	return build(async (source) => {
 		for await (const obj of source) {
-			sendLog(obj.msg, options);
+			const request = await fetch(
+				options.apiURL ?? "https://loggregator.co/api/logger",
+				{
+					method: "POST",
+					body: JSON.stringify({ content: obj.msg }),
+					headers: {
+						"Content-Type": "application/json",
+						Authorization: `Bearer ${options.token}`,
+					},
+				},
+			);
+
+			const body = (await request.json()) as
+				| { success: true }
+				| { success: false; error: string };
+
+			const success = body.success;
+			if (!success)
+				console.warn("[PINO-LOGGREGATOR] request to API failed!", body.error);
 		}
 	});
 }
